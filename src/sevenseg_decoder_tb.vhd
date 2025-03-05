600ebfcde33dd60af419ddecc372f1936a97dd43
mohamedmetwally123 <c27mohamed.metwally@afacademy.af.edu>
2025-03-04 19:15:39 -0700

    lab2

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -53,12 +53,40 @@ begin
 
     test_process : process
     begin
-        w_Hex <= "0000"; wait for 10 ns;
-            assert w_seg_n = "1000000" report "error on 0" severity failure;
-        w_Hex <= "1111"; wait for 10 ns;
-            assert w_seg_n = "0001110" report "error on F" severity failure;
-        w_Hex <= "1010"; wait for 10 ns;
-            assert w_seg_n = "0001000" report "error on A" severity failure;
+  
+  
+    w_Hex <= "0000"; wait for 10 ns;
+        assert (w_seg_n = "1000000") report "Test Fail input 0000" severity failure;
+    w_Hex <= "0001"; wait for 10 ns;
+        assert (w_seg_n = "1111001") report "Test Fail input 0001" severity failure;
+    w_Hex <= "0010"; wait for 10 ns;
+        assert (w_seg_n = "0100100") report "Test Fail input 0010" severity failure;
+    w_Hex <= "0011"; wait for 10 ns;
+        assert (w_seg_n = "0110000") report "Test Fail input 0011" severity failure;
+    w_Hex <= "0100"; wait for 10 ns;
+        assert (w_seg_n = "0011001") report "Test Fail input 0100" severity failure;       
+    w_Hex <= "0101"; wait for 10 ns;
+        assert (w_seg_n = "0010010") report "Test Fail input 0101" severity failure;
+    w_Hex <= "0110"; wait for 10 ns;
+        assert (w_seg_n = "0000010") report "Test Fail input 0110" severity failure;        
+    w_Hex <= "0111"; wait for 10 ns;
+        assert (w_seg_n = "1111000") report "Test Fail input 0111" severity failure;        
+    w_Hex <= "1000"; wait for 10 ns;
+        assert (w_seg_n = "0000000") report "Test Fail input 1000" severity failure;        
+    w_Hex <= "1001"; wait for 10 ns;
+        assert (w_seg_n = "0011000") report "Test Fail input 1001" severity failure;        
+    w_Hex <= "1010"; wait for 10 ns;
+        assert (w_seg_n = "0001000") report "Test Fail input 1010" severity failure;        
+    w_Hex<= "1011"; wait for 10 ns;
+        assert (w_seg_n = "0000011") report "Test Fail input 1011" severity failure;        
+    w_Hex <= "1100"; wait for 10 ns;
+        assert (w_seg_n = "0100111") report "Test Fail input 1100" severity failure;        
+    w_Hex <= "1101"; wait for 10 ns;
+        assert (w_seg_n = "0100001") report "Test Fail input 1101" severity failure;        
+    w_Hex <= "1110"; wait for 10 ns;
+        assert (w_seg_n = "0000110") report "Test Fail input 1110" severity failure;        
+    w_Hex <= "1111"; wait for 10 ns;
+        assert (w_seg_n = "0001110") report "Test Fail input 1111" severity failure;        
         
         wait;
     end process;
